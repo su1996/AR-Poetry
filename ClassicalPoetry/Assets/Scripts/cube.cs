f5670702ccf2b053676b99da0d4d081d22f5c20c
Zzxin9 <71967140@qq.com>
2016-08-11 10:40:38 +0800

    工程文件

---
+++ ClassicalPoetry/Assets/Scripts/cube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Debug.Log ("找到cube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
